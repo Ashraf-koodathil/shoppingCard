5023c3a7fed76ecfd942f10e0a539dc3d18e30d3
Ashraf M <ashrafcvd123@gmail.com>
2024-05-06 07:09:17 +0530

    all are set

--- product.js
+++ product.js
@@ -1,8 +1,29 @@
 const sizeChart = document.querySelector('#char-list')
 const icon = document.querySelector('.fa-chevron-down')
+const sizeAssign = document.querySelector('#char-list')
+const openChart = document.querySelector('#openChart')
+let swapVar;
 
 
-sizeChart.addEventListener('click', () => {
+sizeChart.addEventListener('click',openBox)
+openChart.addEventListener('click',openBox)
+
+function openBox(event) {
+
     sizeChart.classList.toggle('chart-open')
-    icon.remove();
-})
+    icon.style.transform = "rotate(180deg)"
+    if(event.target.matches('h6')){
+        
+        swapVar = sizeAssign.firstElementChild.innerHTML
+
+        sizeAssign.firstElementChild.innerHTML = event.target.innerText
+
+        event.target.innerText = swapVar
+
+        setTimeout(() => {
+            icon.style.transform = "none"
+        },1000)
+        
+    }
+   
+}
