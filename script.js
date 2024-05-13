8d08705e627a4834925f0d9f72a7f933d441356c
Ashraf M <ashrafcvd123@gmail.com>
2024-05-13 16:33:55 +0530

    a file delet and fix

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
 const image = document.getElementById("main-img")
 
-image.addEventListener('mouseover', () => {
-    image.setAttribute('src','images/67017140_43_B.avif');
-})
+try{
+    image.addEventListener('mouseover', () => {
+        image.setAttribute('src','images/67017140_43_B.avif');
+    })
+    
+    image.addEventListener('mouseout', () => {
+        image.setAttribute('src','images/67017140_43-99999999_01.avif');
+    })
+    
+}catch(err){
+    console.log(err)
+}
+    
 
-image.addEventListener('mouseout', () => {
-    image.setAttribute('src','images/67017140_43-99999999_01.avif');
-})
 
+const sizeChart = document.querySelector('#char-list')
+const icon = document.querySelector('.fa-chevron-down')
+const sizeAssign = document.querySelector('#char-list')
+const openChart = document.querySelector('#openChart')
+let swapVar;
+
+
+sizeChart.addEventListener('click',openBox)
+openChart.addEventListener('click',openBox)
+
+function openBox(event) {
+
+    sizeChart.classList.toggle('chart-open')
+    icon.style.transform = "rotate(180deg)"
+    if(event.target.matches('h6')){
+        
+        swapVar = sizeAssign.firstElementChild.innerHTML
+
+        sizeAssign.firstElementChild.innerHTML = event.target.innerText
+
+        event.target.innerText = swapVar
+
+        setTimeout(() => {
+            icon.style.transform = "none"
+        },1000)
+        
+    }
+   
+}
 
 
 
